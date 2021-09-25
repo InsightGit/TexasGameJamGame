ece038546aa21f0e628f6434e03ccb8076e71611
Bobby Youstra <bobbyyoustra@protonmail.com>
2021-09-25 12:37:56 -0500

    Got TileRunner without edge-cases working!

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -10,106 +10,82 @@ public class TileManager : MonoBehaviour
     public Vector2Int tileSize = new Vector2Int(20, 20);
     public int numberOfTileStrips = 10;
     public int tileStripLength = 100;
-    public int frameStripUpdateSpeed = 30;
+    public float frameStripUpdateSpeedInSeconds = 1.0f;
     public List<TileBase> tilesToUse;
+    public TileBase grassTile;
+    public List<int> tileProbablities;
 
-    private int mFrameNumber = 0;
     private Tilemap mTilemap;
     
     // Start is called before the first frame update
     void Start()
     {
         mTilemap = GetComponent<Tilemap>();
+        
+        Assert.AreEqual(tilesToUse.Count, tileProbablities.Count);
+
+        StartCoroutine("AddNewAndShiftTileStrips");
     }
 
-    void AddNewAndShiftTileStrips()
+    private IEnumerator AddNewAndShiftTileStrips()
     {
-        Vector3Int currentTileStripHead = firstTileStripPosition;
-
-        List<TileBase> newTileStrip = new List<TileBase>();
-        Queue<List<TileBase>> tileStrips = new Queue<List<TileBase>>();
-
-        for (int y = 0; tileStripLength / tileSize.y > y; ++y)
+        while (true)
         {
-            newTileStrip.Add(tilesToUse[Random.Range(0, tilesToUse.Count)]);
-        }
+            Vector3Int currentTileStripHead = firstTileStripPosition;
 
-        tileStrips.Enqueue(newTileStrip);
-        
-        for (int i = 0; numberOfTileStrips - 1 > i; ++i)
-        {
-            List<TileBase> tileStripLine = new List<TileBase>();
-            
-            for (int y = 0; tileStripLength / tileSize.y > y; ++y)
+            Queue<TileBase> pastTileStrip = new Queue<TileBase>();
+
+            for(int y = 0; tileStripLength / tileSize.y > y; ++y)
             {
-                Vector3Int tilePosition = new Vector3Int(currentTileStripHead.x - (i * tileSize.x),
-                    currentTileStripHead.y + (y * tileSize.y), 0);
-                
-                tileStripLine.Add(mTilemap.GetTile(tilePosition));
-                //newTileStrip.Add();
-            }
-            
-            tileStrips.Enqueue(tileStripLine);
-        }
-        
-        Assert.AreEqual(numberOfTileStrips, tileStrips.Count);
-        
-        for(int i = 0; numberOfTileStrips > i; ++i)
-        {
-            List<TileBase> tileStripLine = tileStrips.Dequeue();
-            
-            for (int y = 0; tileStripLength > y; ++y)
-            {
-                int currentTileIndex = y / tileSize.y;
-                TileBase currentTile = tileStripLine[currentTileIndex];
+                TileBase randomTile = null;
 
-                for (int x = 0; tileSize.x > x; ++x)
+                for (int i = 0; tileProbablities.Count > i; ++i)
                 {
-                    Vector3Int tilePosition = new Vector3Int(currentTileStripHead.x - x,
-                        currentTileStripHead.y + y, 0);
-                    
-                    mTilemap.SetTile(tilePosition, currentTile);
-                }
-            }
-        }
-    }
-    
-    
-    // Update is called once per frame
-    void Update()
-    {
-        ++mFrameNumber;
-
-        if (frameStripUpdateSpeed >= mFrameNumber)
-        {
-            AddNewAndShiftTileStrips();
-            mFrameNumber = 0;
-        }
-    }
-    
-            /*for (int i = 0; numberOfTileStrips > i; ++i)
-        {
-            currentTileStripHead.x = firstTileStripPosition.x - (tileSize.x * i);
-
-            int pastTileId = 0;
-            Vector2Int pastTileVector = new Vector2Int(-1, -1);
-
-            for (int y = 0; tileStripLength > y; ++y)
-            {
-                Vector2Int tileVector = new Vector2Int(currentTileStripHead.x, y % tileSize.y);
-
-                if (pastTileVector != tileVector)
-                {
-                    Assert.IsTrue(tilesToUse.Count > 0);
-
-                    if (i == 0)
+                    if (Random.Range(0, tileProbablities[i]) == 0)
                     {
-                        pastTileId = Random.Range(0, tilesToUse.Count);
-                        
+                        randomTile = tilesToUse[i];
+                        break;
                     }
                 }
+
+                if (randomTile == null)
+                {
+                    randomTile = grassTile;
+                }
                 
-                
+                pastTileStrip.Enqueue(randomTile);
             }
-        }*/
+        
+            TileBase currentTile = null;
+        
+            for (int i = 0; numberOfTileStrips > i; ++i)
+            {
+                Assert.AreEqual(tileStripLength / tileSize.y, pastTileStrip.Count);
+                
+                for (int y = 0; tileStripLength > y; ++y)
+                {
+                    if (y % tileSize.y == 0)
+                    {
+                        Vector3Int tilePosition = new Vector3Int(currentTileStripHead.x - (i * tileSize.x), 
+                                                                 currentTileStripHead.y + y, 0);
+                        TileBase tile = mTilemap.GetTile(tilePosition);
+                        
+                        currentTile = pastTileStrip.Dequeue();
+
+                        pastTileStrip.Enqueue(tile);
+                    }
+                
+                    for (int x = 0; tileSize.x > x; ++x)
+                    {
+                        Vector3Int tilePosition = new Vector3Int(currentTileStripHead.x - (i * tileSize.x) - x, 
+                            currentTileStripHead.y + y, 0);
+                    
+                        mTilemap.SetTile(tilePosition, currentTile);
+                    }
+                }
+            }
+
+            yield return new WaitForSeconds(frameStripUpdateSpeedInSeconds);
+        }
+    }
 }
