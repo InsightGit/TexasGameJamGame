af3535e1751e5ee25b7369177a0cd14364d923cd
Bobby Youstra <bobbyyoustra@protonmail.com>
2021-09-25 17:34:10 -0500

    Before changing bounds

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,12 +7,19 @@ using UnityEngine.Tilemaps;
 public class PlayerScript : MonoBehaviour
 {
     public Vector2 tileSize = new Vector2(20 * 1.28f, 20 * 1.28f);
-    public TileManager tileManager;
-    public float jumpingLengthSeconds = 0.4f;
 
-    private Vector2 mMovement;
-    private bool mJumping = false;
+    public FadeTransitionManager fadeTransitionManager;
+    public ParentScript parentEnemy;
+    public TileManager tileManager;
+    
+    public float jumpingLengthSeconds = 0.75f;
+    public int maxObstacleHitsBeforeParentMinigame = 3;
+
     private int mCurrentTileStrip;
+    private Vector2 mMovement;
+    private int mObstacleHits = 0;
+    private bool mJumping = false;
+    private TileManager.TileType mPastTileType = TileManager.TileType.GRASS;
 
     // Start is called before the first frame update
     void Start()
@@ -27,18 +34,36 @@ public class PlayerScript : MonoBehaviour
         {
             TileManager.TileType currentTile = tileManager.getPlayerTile(transform.position);
 
-            switch (currentTile)
+            if (currentTile != mPastTileType)
             {
-                case TileManager.TileType.MINIGAME:
-                    Debug.Log("Checkpoint Minigame time!");
-                    break;
-                case TileManager.TileType.GRASS:
-                    break;
-                case TileManager.TileType.OBSTACLE:
-                    Debug.Log("Parent Minigame time!");
-                    break;
+                switch (currentTile)
+                {
+                    case TileManager.TileType.MINIGAME:
+                        Debug.Log("Checkpoint Minigame time!");
+                        break;
+                    case TileManager.TileType.GRASS:
+                        break;
+                    case TileManager.TileType.OBSTACLE:
+                        ++mObstacleHits;
+
+                        parentEnemy.MoveCloser();
+                        
+                        if (maxObstacleHitsBeforeParentMinigame > mObstacleHits)
+                        {
+                            Debug.Log("The parent is getting closer!");
+                        }
+                        else
+                        {
+                            Debug.Log("Parent Minigame time!");
+                            
+                            fadeTransitionManager.StartFadeOut();
+                        }
+                        break;
+                }
+                
+                mPastTileType = currentTile;
             }
-            
+
             if (Input.GetButtonDown("Jump"))
             {
                 mJumping = true;
---
+++ Assets/Scripts/RunnerGameState.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerGameState
+{
+    public static bool paused;
+}
